9bd228349cf2f3b1ebe92602c02fce803202319d
Bhavik Parmar <bhavik2936@gmail.com>
2021-06-13 19:46:56 +0530

    overrided devise controllers
    overrided devise controllers in versioned manner

---
+++ app/controllers/api/v1/managers/confirmations_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::Managers::ConfirmationsController < Devise::ConfirmationsController
+  respond_to :json
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :managers
+  namespace :api do
+    namespace :v1 do
+      devise_for :managers, controllers: {
+        sessions: 'api/v1/managers/sessions',
+        registrations: 'api/v1/managers/registrations',
+        confirmations: 'api/v1/managers/confirmations',
+        passwords: 'api/v1/managers/passwords'
+      }
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
