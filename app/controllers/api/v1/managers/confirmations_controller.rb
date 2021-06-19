e2f7b4bed1b992a5b736ecdfddd978d4bb726dcf
Bhavik Parmar <bhavik2936@gmail.com>
2021-06-19 13:58:55 +0530

    added confirmation redirection url
    after confirmation redirects to specified url

--- app/controllers/api/v1/managers/confirmations_controller.rb
+++ app/controllers/api/v1/managers/confirmations_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::Managers::ConfirmationsController < Devise::ConfirmationsController
   respond_to :json
+
+  def after_confirmation_path_for(resource_name, resource)
+    ENV['REACT_APP_URL']
+   end
 end
