239a5a7555c332cbea7a3c2468955decfd958597
Bhavik Parmar <bhavik2936@gmail.com>
2021-06-22 16:40:22 +0530

    added email templates
    modified devise templates to react app url

--- app/controllers/api/v1/managers/confirmations_controller.rb
+++ app/controllers/api/v1/managers/confirmations_controller.rb
@@ -1,7 +1,3 @@
 class Api::V1::Managers::ConfirmationsController < Devise::ConfirmationsController
   respond_to :json
-
-  def after_confirmation_path_for(resource_name, resource)
-    ENV['REACT_APP_URL']
-   end
 end
